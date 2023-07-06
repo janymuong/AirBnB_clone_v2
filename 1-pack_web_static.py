c3009b527932cc1ee5ee731cb9105d34c33d7441
Jany Muong <me.roumuong@gmail.com>
2023-07-06 21:09:18 +0300

    tar gz: [correction - stuff] compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,25 +4,21 @@ create tarball artifact of static files in local
 '''
 
 from fabric.api import local
-from datetime import datetime
+import time
 import os
 
 
 def do_pack():
-    '''function:
-    generates a .tgz archive from the contents of the web_static folder
     '''
-
-    now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-
-    artifact = f'versions/web_static_{now}.tgz'
+    A function that generaees a .tgx archive from the web_static folder
+    '''
     if not os.path.exists('versions'):
-        local('mkdir -p versions')
+        os.makedirs('versions')
 
-    fab_stat = local(f'tar -czvf {artifact} web_static')
-    if fab_stat.succeeded:
-        size = os.path.getsize(artifact)
-        print(f'web_static packed: {artifact} -> {size}Bytes')
+    filename = time.strftime("%Y%m%d%H%M%S")
+    artifact = "versions/web_static_{}.tgz".format(filename)
+    try:
+        local("tar -cvzf {} web_static".format(artifact))
         return artifact
-    else:
+    except Exception:
         return None
