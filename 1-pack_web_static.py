38c6761ebb17b4d89dd4c255568afd599ac19185
Jany Muong <me.roumuong@gmail.com>
2023-07-06 23:37:07 +0300

    tar gz: [match - use archive_path variable] compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,15 +14,15 @@ def do_pack():
     '''
     now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
 
-    artifact = f'versions/web_static_{now}.tgz'
-    print(f'Packing web_static to {artifact}')
+    archive_path = f'versions/web_static_{now}.tgz'
+    print(f'Packing web_static to {archive_path}')
     if not os.path.exists('versions'):
         local('mkdir -p versions')
 
-    fab_stat = local(f'tar -cvzf {artifact} web_static')
+    fab_stat = local(f'tar -cvzf {archive_path} web_static')
     if fab_stat.succeeded:
-        size = os.path.getsize(artifact)
-        print(f'web_static packed: {artifact} -> {size}Bytes')
-        return artifact
+        size = os.path.getsize(archive_path)
+        print(f'web_static packed: {archive_path} -> {size}Bytes')
+        return archive_path
     else:
         return None
