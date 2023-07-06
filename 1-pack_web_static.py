5bad7bea00d6f872cc8178ca9cacf5d78e5f8eb0
Jany Muong <me.roumuong@gmail.com>
2023-07-06 20:42:59 +0300

    tar gz: [correction - 1] compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -19,7 +19,7 @@ def do_pack():
     if not os.path.exists('versions'):
         local('mkdir -p versions')
 
-    fab_stat = local(f'tar -czvf {artifact} web_static')
+    fab_stat = local(f'tar -cvzf {artifact} web_static')
     if fab_stat.succeeded:
         size = os.path.getsize(artifact)
         print(f'web_static packed: {artifact} -> {size}Bytes')
