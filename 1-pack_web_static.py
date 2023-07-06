18845fcc5754883b21da294f309a5b62d5737167
Jany Muong <me.roumuong@gmail.com>
2023-07-06 21:35:11 +0300

    set artifact permission: -rw-rw-r--

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,21 +4,26 @@ create tarball artifact of static files in local
 '''
 
 from fabric.api import local
-import time
+from datetime import datetime
 import os
 
 
 def do_pack():
+    '''function:
+    generates a .tgz archive from the contents of the web_static folder
     '''
-    A function that generaees a .tgx archive from the web_static folder
-    '''
-    if not os.path.exists('versions'):
-        os.makedirs('versions')
 
-    filename = time.strftime("%Y%m%d%H%M%S")
-    artifact = "versions/web_static_{}.tgz".format(filename)
-    try:
-        local("tar -cvzf {} web_static".format(artifact))
+    now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+
+    artifact = f'versions/web_static_{now}.tgz'
+    if not os.path.exists('versions'):
+        local('mkdir -p versions')
+
+    fab_stat = local(f'tar -czvf {artifact} web_static')
+    if fab_stat.succeeded:
+        local(f'chmod 664 {artifact}')
+        size = os.path.getsize(artifact)
+        print(f'web_static packed: {artifact} -> {size}Bytes')
         return artifact
-    except Exception:
+    else:
         return None
