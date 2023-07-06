28c8506c03ddae10808ba1fbb7086bd14ee8ddf9
Jany Muong <me.roumuong@gmail.com>
2023-07-06 22:54:38 +0300

    tar gz: [correction - alt] compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,17 +13,17 @@ def do_pack():
     generates a .tgz archive from the contents of the web_static folder
     '''
 
-    now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    now = datetime.utcnow()
+    artifact_name = 'web_static_' + now.strftime('%Y%m%d%H%M%S') + '.tgz'
+    artifact_path = 'versions/' + artifact_name
 
-    artifact = f'versions/web_static_{now}.tgz'
     if not os.path.exists('versions'):
         local('mkdir -p versions')
 
-    fab_stat = local(f'tar -czvf {artifact} web_static')
-    if fab_stat.succeeded:
-        local(f'chmod 664 {artifact}')
-        size = os.path.getsize(artifact)
-        print(f'web_static packed: {artifact} -> {size}Bytes')
-        return artifact
-    else:
+    result = local('tar -cvzf {} web_static'.format(artifact_path))
+    if result.failed:
         return None
+
+    size = os.path.getsize(artifact_path)
+    print('web_static packed: {} -> {}Bytes'.format(artifact_path, size))
+    return artifact_path
