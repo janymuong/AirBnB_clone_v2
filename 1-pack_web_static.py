9cf8aa5c0efd3aeb658b761e7a89964ba47ebcb6
Jany Muong <me.roumuong@gmail.com>
2023-07-06 19:57:37 +0300

    tar gz: [correction - check path of versions]compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -16,7 +16,8 @@ def do_pack():
     now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
 
     artifact = f'versions/web_static_{now}.tgz'
-    local('mkdir -p versions')
+    if not os.path.exists('versions'):
+        local('mkdir -p versions')
     fab_stat = local(f'tar -czvf {artifact} web_static')
 
     if fab_stat.succeeded:
