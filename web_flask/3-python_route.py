88fbc488cc8f9ff720dc7b7de56bfcf5344836ca
Jany Muong <me.roumuong@gmail.com>
2023-07-22 13:34:54 +0300

    use the `defaults` argument to set the default value for url param text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''module - multi-routes:
+define index(), and plus some other endpoints
+'''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''return `Hello, World` text'''
+    return f'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''display “HBNB”'''
+    return f'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def url_text(text):
+    '''
+    pass URL parameters to endpoint control
+    the 'text' variable will be of type string
+    '''
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    '''return `Python is cool` text with default or custom text'''
+    return f'Python {text.replace("_", " ")}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
