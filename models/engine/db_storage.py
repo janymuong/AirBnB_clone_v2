d254947c8d57a003d1cfe0da5b4ad6f2756ccb2f
Jany Muong <me.roumuong@gmail.com>
2023-07-23 22:29:41 +0300

    DBStorage-SQLAlchemy: close scoped_session object

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -83,4 +83,4 @@ class DBStorage():
         '''close:
         Calls remove() method on the private session attribute (self.__session)
         '''
-        self.__session.remove()
+        self.__session.close()
