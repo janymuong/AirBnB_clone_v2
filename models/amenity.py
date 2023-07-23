fdb9e04fc12f1213195cc12b188573a3186f0144
Jany Muong <me.roumuong@gmail.com>
2023-07-23 19:31:36 +0300

    refactor models

--- models/amenity.py
+++ models/amenity.py
@@ -8,6 +8,9 @@ from models.base_model import BaseModel, Base
 
 
 class Amenity(BaseModel, Base):
+    '''class for amenity: 
+    like services eg WIFI
+    '''
     __tablename__ = 'amenities'
 
     name = Column(String(128), nullable=False)
